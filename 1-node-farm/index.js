35a1f6c22b465c7225ff430519355cd000f3eddc
tanhoang0710 <tanhoang0710@gmail.com>
2022-09-05 15:51:18 +0700

    015 HTML Templating Filling the Templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,6 +3,34 @@ const http = require('http');
 const url = require('url');
 
 // SERVER
+const replaceTemplate = (temp, product) => {
+	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+
+	if (!product.organic)
+		output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+	return output;
+};
+
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	'utf-8'
+);
+const tempCard = fs.readFileSync(
+	`${__dirname}/templates/template-card.html`,
+	'utf-8'
+);
+
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
@@ -10,15 +38,32 @@ const dataObject = JSON.parse(data);
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
 
+	// Overview page
 	if (pathName === '/' || pathName === '/overview') {
-		res.end('This is the OVERVIEW');
+		res.writeHead(200, {
+			'Content-type': 'text/html',
+		});
+
+		const cardsHtml = dataObject
+			.map((el) => replaceTemplate(tempCard, el))
+			.join('');
+
+		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+		res.end(output);
+
+		// Product page
 	} else if (pathName === '/product') {
 		res.end('This is the PRODUCT');
+
+		// API
 	} else if (pathName === '/api') {
 		res.writeHead(200, {
 			'Content-type': 'application/json',
 		});
 		res.end(dataObject);
+
+		// NOT FOUND
 	} else {
 		res.writeHead(404, {
 			'Content-type': 'text/html',
