d8c3fa1e73b896784f157a3f5dad3b7439aa4a83
tanhoang0710 <tanhoang0710@gmail.com>
2022-09-08 22:24:34 +0700

    020 Using Modules 3 3rd Party Modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 // SERVER
@@ -21,6 +24,18 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
+
+const slugs = dataObject.map((el) => {
+	return slugify(el.productName, { lower: true });
+});
+
+console.log(slugs);
+
+console.log(
+	slugify('Fresh Avocados', {
+		lower: true,
+	})
+);
 
 const server = http.createServer((req, res) => {
 	const { query, pathname } = url.parse(req.url, true);
