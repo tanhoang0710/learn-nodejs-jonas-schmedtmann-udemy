8d1a90404d6c00d4043f87717ef4bccebbebd7df
tanhoang0710 <tanhoang0710@gmail.com>
2022-09-27 09:43:49 +0700

    067 Environment Variables

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,13 @@
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: './config.env',
+});
+
 const app = require('./app');
 
 // 4) Start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
