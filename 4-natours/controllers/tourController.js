11c1cfac438c695746a7c2989ba7f620a7ca831a
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-01 10:54:54 +0700

    089 Reading Documents

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,24 +1,41 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        // results: tours.length,
-        // data: {
-        //     tours,
-        // },
-    });
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find({});
+
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours,
+            },
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: error,
+        });
+    }
 };
 
-exports.getTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        // data: {
-        //     tour,
-        // },
-    });
+exports.getTour = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id);
+        // Tour.findOne({_id: req.params.id})
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
 
 exports.createTour = async (req, res) => {
