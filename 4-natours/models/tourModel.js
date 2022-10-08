b3228b64289d07592810b8ad6a61a4ba6a3f7a7f
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-08 14:12:26 +0700

    108 Data Validation Custom Validators

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -16,6 +17,10 @@ const tourSchema = new mongoose.Schema(
                 10,
                 'A tour name must have more or equal 40 characters',
             ],
+            // validator: [
+            //     validator.isAlpha,
+            //     'Tour name must only contains characters',
+            // ],
         },
         slug: String,
         duration: {
@@ -49,7 +54,18 @@ const tourSchema = new mongoose.Schema(
             type: Number,
             required: [true, 'A tour must have a price'],
         },
-        priceDiscount: Number,
+        priceDiscount: {
+            type: Number,
+            validate: {
+                validator: function (val) {
+                    // chỉ có tác dụng với create, ko có tác dụng với update
+                    // this only points to current doc on NEW document creation
+                    return val < this.price;
+                },
+                message:
+                    'Discount price ({VALUE}) should be below regular price',
+            },
+        },
         summary: {
             type: String,
             trim: true,
