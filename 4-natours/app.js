ab1722c646eb7f22c637a5c73e207ebbe561cf98
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-09 18:11:33 +0700

    114 Better Errors and Refactoring

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -24,27 +26,15 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on this server`,
-    // });
-
-    const err = new Error(`Can't find ${req.originalUrl} on this server`);
-    err.status = 'fail';
-    err.statusCode = 404;
+    const err = new AppError(
+        `Can't find ${req.originalUrl} on this server`,
+        404
+    );
 
     next(err);
 });
 
 // express tu biet day la error handling middleware
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
