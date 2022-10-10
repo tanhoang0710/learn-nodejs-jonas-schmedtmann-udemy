0596c4d65bc741e5821ba53a6dbe099b2d6032be
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-10 12:00:21 +0700

    115 Catching Errors in Async Functions

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const APIFeature = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 
 exports.aliasTopTour = async (req, res, next) => {
     req.query.limit = '5';
@@ -8,349 +9,300 @@ exports.aliasTopTour = async (req, res, next) => {
     next();
 };
 
-exports.getAllTours = async (req, res) => {
-    try {
-        // BUILD QUERY
-        // 1A) Filtering
-        // const queryObj = { ...req.query };
-        // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+exports.getAllTours = catchAsync(async (req, res, next) => {
+    // BUILD QUERY
+    // 1A) Filtering
+    // const queryObj = { ...req.query };
+    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
-        // excludedFields.forEach((el) => delete queryObj[el]);
+    // excludedFields.forEach((el) => delete queryObj[el]);
 
-        // // 1B) Advanced filtering
-        // let queryStr = JSON.stringify(queryObj);
-        // queryStr = queryStr.replace(
-        //     /\b(gte|gt|lte|lt)\b/g,
-        //     (match) => `$${match}`
-        // );
-        // let query = Tour.find(JSON.parse(queryStr));
+    // // 1B) Advanced filtering
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(
+    //     /\b(gte|gt|lte|lt)\b/g,
+    //     (match) => `$${match}`
+    // );
+    // let query = Tour.find(JSON.parse(queryStr));
 
-        // const query = await Tour.find()
-        //     .where('duration')
-        //     .equals(5)
-        //     .where('difficulty')
-        //     .equals('easy');
+    // const query = await Tour.find()
+    //     .where('duration')
+    //     .equals(5)
+    //     .where('difficulty')
+    //     .equals('easy');
 
-        // 2) Sorting
-        // if (req.query.sort) {
-        //     const sortBy = req.query.sort.split(',').join(' ');
-        //     query = query.sort(sortBy);
-        // } else {
-        //     query = query.sort('-createdAt');
-        // }
+    // 2) Sorting
+    // if (req.query.sort) {
+    //     const sortBy = req.query.sort.split(',').join(' ');
+    //     query = query.sort(sortBy);
+    // } else {
+    //     query = query.sort('-createdAt');
+    // }
 
-        // 3) Field limiting
-        // if (req.query.fields) {
-        //     const fields = req.query.fields.split(',').join(' ');
-        //     query = query.select(fields);
-        // } else {
-        //     query = query.select('-__v');
-        // }
+    // 3) Field limiting
+    // if (req.query.fields) {
+    //     const fields = req.query.fields.split(',').join(' ');
+    //     query = query.select(fields);
+    // } else {
+    //     query = query.select('-__v');
+    // }
 
-        // 4) Pagination
-        // const page = req.query.page * 1 || 1;
-        // const limit = req.query.limit * 1 || 100;
-        // const skip = (page - 1) * limit;
+    // 4) Pagination
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
 
-        // query = query.skip(skip).limit(limit);
+    // query = query.skip(skip).limit(limit);
 
-        // if (req.query.page) {
-        //     const numTours = await Tour.countDocuments();
-        //     if (skip >= numTours) throw new Error('This page does not exist');
-        // }
+    // if (req.query.page) {
+    //     const numTours = await Tour.countDocuments();
+    //     if (skip >= numTours) throw new Error('This page does not exist');
+    // }
 
-        // EXECUTE QUERY
-        const features = new APIFeature(Tour.find(), req.query)
-            .filter()
-            .sort()
-            .paginate();
-        const tours = await features.query;
+    // EXECUTE QUERY
+    const features = new APIFeature(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .paginate();
+    const tours = await features.query;
 
-        // SEND RESPONSE
-        res.status(200).json({
-            status: 'success',
-            results: tours.length,
-            data: {
-                tours,
-            },
-        });
-    } catch (error) {
-        res.status(404).json({
-            status: 'fail',
-            message: error,
-        });
-    }
-};
+    // SEND RESPONSE
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours,
+        },
+    });
+});
 
-exports.getTour = async (req, res) => {
-    try {
-        const tour = await Tour.findById(req.params.id);
-        // Tour.findOne({_id: req.params.id})
+exports.getTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({_id: req.params.id})
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                tour,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err,
-        });
-    }
-};
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour,
+        },
+    });
+});
 
-exports.createTour = async (req, res) => {
-    try {
-        const newTour = await Tour.create(req.body);
+exports.createTour = catchAsync(async (req, res, next) => {
+    const newTour = await Tour.create(req.body);
 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour,
-            },
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: 'fail',
-            message: 'Invalid data sent',
-        });
-    }
-};
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour,
+        },
+    });
+});
 
-exports.updateTour = async (req, res) => {
-    try {
-        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-        res.status(200).json({
-            status: 'success',
-            data: {
-                tour,
-            },
-        });
-    } catch (error) {
-        res.status(404).json({
-            status: 'fail',
-            message: error,
-        });
-    }
-};
+exports.updateTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour,
+        },
+    });
+});
 
-exports.deleteTour = async (req, res) => {
-    try {
-        await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = catchAsync(async (req, res) => {
+    await Tour.findByIdAndDelete(req.params.id);
 
-        res.status(204).json({
-            status: 'success',
-            data: null,
-        });
-    } catch (error) {
-        res.status(404).json({
-            status: 'fail',
-            message: error,
-        });
-    }
-};
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+});
 
-exports.getTourStats = async (req, res) => {
-    try {
-        const stats = await Tour.aggregate([
-            {
-                $match: {
-                    ratingAverage: {
-                        $gte: 4.5,
-                    },
+exports.getTourStats = catchAsync(async (req, res, next) => {
+    const stats = await Tour.aggregate([
+        {
+            $match: {
+                ratingAverage: {
+                    $gte: 4.5,
                 },
-            }, // lay ra nhung tour co danh gia >= 4.5
-            // roi nhom theo ID
+            },
+        }, // lay ra nhung tour co danh gia >= 4.5
+        // roi nhom theo ID
 
-            // ko group by
+        // ko group by
+        // {
+        //     $group: {
+        //         _id: null,
+        //         num: {
+        //             $sum: 1,
+        //         }, // moi tour di qua pipeline nay se tang num len 1
+        //         numRatings: {
+        //             $sum: '$ratingsQuantity',
+        //         },
+        //         avgRating: {
+        //             $avg: '$ratingsAverage',
+        //         },
+        //         avgPrice: {
+        //             $avg: '$price',
+        //         },
+        //         minPrice: {
+        //             $min: '$price',
+        //         },
+        //         maxPrice: {
+        //             $max: '$price',
+        //         },
+        //     },
+        // },
+
+        // group by difficulty
+        {
+            $group: {
+                _id: { $toUpper: '$difficulty' },
+                num: {
+                    $sum: 1,
+                }, // moi tour di qua pipeline nay se tang num len 1
+                numRatings: {
+                    $sum: '$ratingsQuantity',
+                },
+                avgRating: {
+                    $avg: '$ratingAverage',
+                },
+                avgPrice: {
+                    $avg: '$price',
+                },
+                minPrice: {
+                    $min: '$price',
+                },
+                maxPrice: {
+                    $max: '$price',
+                },
+            },
+        },
+
+        // group theo ratingAverage
+        // {
+        //     $group: {
+        //         _id: '$ratingAverage',
+        //         num: {
+        //             $sum: 1,
+        //         }, // moi tour di qua pipeline nay se tang num len 1
+        //         numRatings: {
+        //             $sum: '$ratingsQuantity',
+        //         },
+        //         avgRating: {
+        //             $avg: '$ratingAverage',
+        //         },
+        //         avgPrice: {
+        //             $avg: '$price',
+        //         },
+        //         minPrice: {
+        //             $min: '$price',
+        //         },
+        //         maxPrice: {
+        //             $max: '$price',
+        //         },
+        //     },
+        // },
+
+        // sort theo field moi'
+        {
+            $sort: {
+                avgPrice: 1, // 1 for ascending
+            },
+        },
+
+        // match lan nua chi lay document co difficuly khac EASY
+        // {
+        //     $match: {
+        //         _id: {
+        //             $ne: 'EASY',
+        //         },
+        //     },
+        // },
+    ]);
+
+    res.status(200).json({
+        status: 'success',
+        data: { stats },
+    });
+});
+
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+        {
+            // de tach field cua document ma co nhieu gia tri ra nhieu document ma field do chi co 1
+            // vd 1 document:
             // {
-            //     $group: {
-            //         _id: null,
-            //         num: {
-            //             $sum: 1,
-            //         }, // moi tour di qua pipeline nay se tang num len 1
-            //         numRatings: {
-            //             $sum: '$ratingsQuantity',
-            //         },
-            //         avgRating: {
-            //             $avg: '$ratingsAverage',
-            //         },
-            //         avgPrice: {
-            //             $avg: '$price',
-            //         },
-            //         minPrice: {
-            //             $min: '$price',
-            //         },
-            //         maxPrice: {
-            //             $max: '$price',
-            //         },
-            //     },
-            // },
-
-            // group by difficulty
-            {
-                $group: {
-                    _id: { $toUpper: '$difficulty' },
-                    num: {
-                        $sum: 1,
-                    }, // moi tour di qua pipeline nay se tang num len 1
-                    numRatings: {
-                        $sum: '$ratingsQuantity',
-                    },
-                    avgRating: {
-                        $avg: '$ratingAverage',
-                    },
-                    avgPrice: {
-                        $avg: '$price',
-                    },
-                    minPrice: {
-                        $min: '$price',
-                    },
-                    maxPrice: {
-                        $max: '$price',
-                    },
-                },
-            },
-
-            // group theo ratingAverage
+            //     name: 'name',
+            //     images: [
+            //         'image1',
+            //         'image2',
+            //         'image3',
+            //     ]
+            // }
+            // sau khi unwind theo images se thanh 3 document
             // {
-            //     $group: {
-            //         _id: '$ratingAverage',
-            //         num: {
-            //             $sum: 1,
-            //         }, // moi tour di qua pipeline nay se tang num len 1
-            //         numRatings: {
-            //             $sum: '$ratingsQuantity',
-            //         },
-            //         avgRating: {
-            //             $avg: '$ratingAverage',
-            //         },
-            //         avgPrice: {
-            //             $avg: '$price',
-            //         },
-            //         minPrice: {
-            //             $min: '$price',
-            //         },
-            //         maxPrice: {
-            //             $max: '$price',
-            //         },
-            //     },
-            // },
-
-            // sort theo field moi'
-            {
-                $sort: {
-                    avgPrice: 1, // 1 for ascending
-                },
-            },
-
-            // match lan nua chi lay document co difficuly khac EASY
+            //     name: 'name',
+            //     images: 'image1'
+            // }
             // {
-            //     $match: {
-            //         _id: {
-            //             $ne: 'EASY',
-            //         },
-            //     },
-            // },
-        ]);
+            //     name: 'name',
+            //     images: 'image2'
+            // }
+            // {
+            //     name: 'name',
+            //     images: 'image3'
+            // }
+            $unwind: '$startDates',
+        },
+        {
+            $match: {
+                startDates: {
+                    $gte: new Date(`${year}-01-01`),
+                    $lte: new Date(`${year}-12-31`),
+                },
+            },
+        },
+        {
+            $group: {
+                _id: {
+                    $month: '$startDates',
+                },
+                numToursStarts: {
+                    $sum: 1,
+                },
+                tours: {
+                    $push: '$name',
+                },
+            },
+        },
+        {
+            $addFields: {
+                month: '$_id',
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+            },
+        },
+        {
+            $sort: {
+                numToursStarts: -1,
+            },
+        },
+        {
+            $limit: 12,
+        },
+    ]);
 
-        res.status(200).json({
-            status: 'success',
-            data: { stats },
-        });
-    } catch (error) {
-        res.status(404).json({
-            status: 'fail',
-            message: error,
-        });
-    }
-};
-
-exports.getMonthlyPlan = async (req, res) => {
-    try {
-        const year = req.params.year * 1;
-
-        const plan = await Tour.aggregate([
-            {
-                // de tach field cua document ma co nhieu gia tri ra nhieu document ma field do chi co 1
-                // vd 1 document:
-                // {
-                //     name: 'name',
-                //     images: [
-                //         'image1',
-                //         'image2',
-                //         'image3',
-                //     ]
-                // }
-                // sau khi unwind theo images se thanh 3 document
-                // {
-                //     name: 'name',
-                //     images: 'image1'
-                // }
-                // {
-                //     name: 'name',
-                //     images: 'image2'
-                // }
-                // {
-                //     name: 'name',
-                //     images: 'image3'
-                // }
-                $unwind: '$startDates',
-            },
-            {
-                $match: {
-                    startDates: {
-                        $gte: new Date(`${year}-01-01`),
-                        $lte: new Date(`${year}-12-31`),
-                    },
-                },
-            },
-            {
-                $group: {
-                    _id: {
-                        $month: '$startDates',
-                    },
-                    numToursStarts: {
-                        $sum: 1,
-                    },
-                    tours: {
-                        $push: '$name',
-                    },
-                },
-            },
-            {
-                $addFields: {
-                    month: '$_id',
-                },
-            },
-            {
-                $project: {
-                    _id: 0,
-                },
-            },
-            {
-                $sort: {
-                    numToursStarts: -1,
-                },
-            },
-            {
-                $limit: 12,
-            },
-        ]);
-
-        res.status(200).json({
-            status: 'success',
-            data: { plan },
-        });
-    } catch (error) {
-        res.status(404).json({
-            status: 'fail',
-            message: error,
-        });
-    }
-};
+    res.status(200).json({
+        status: 'success',
+        data: { plan },
+    });
+});
