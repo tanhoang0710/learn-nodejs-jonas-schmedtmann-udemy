b910ae58be35d5488ee3f3ec545ebacf4f644206
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-11 11:56:46 +0700

    122 Catching Uncaught Exceptions

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down');
+    console.log(err);
+    process.exit(1);
+});
+
 dotenv.config({
     path: './config.env',
 });
@@ -30,8 +36,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-    console.log(err.name, err.message);
     console.log('UNHANDLER REJECTION! Shutting down');
+    console.log(err.name, err.message);
     server.close(() => {
         process.exit(1);
     });
