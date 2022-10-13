c8994926d767675e987fa2cccd48bbc9701e720d
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-13 10:12:44 +0700

    129 Logging in Users

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,6 +1,12 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = (id) =>
+    jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
 exports.signup = catchAsync(async (req, res, next) => {
     const { name, email, password, passwordConfirm } = req.body;
@@ -12,9 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordConfirm,
     });
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
-    });
+    const token = signToken(newUser._id);
 
     res.status(201).json({
         status: 'success',
@@ -22,5 +26,30 @@ exports.signup = catchAsync(async (req, res, next) => {
         data: {
             user: newUser,
         },
+    });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password', 400));
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({
+        email,
+    }).select('+password'); // vi ben model đã ẩn field password khi find nên muốn lấy thêm field password ở đây cầm thêm method select và dấu cộng là để thêm field password vào các trường của User, nếu ko có + thì chỉ lấy ra nguyên field password
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    // 3) If everything is ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token,
     });
 });
