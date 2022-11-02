51d1f595120dd876ebf97fe04d40fb437f825025
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-02 14:51:10 +0700

    149 Modelling Tour Guides_ Embedding

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -114,6 +115,7 @@ const tourSchema = new mongoose.Schema(
                 day: Number,
             },
         ],
+        guides: Array,
     },
     {
         toJSON: {
@@ -137,6 +139,14 @@ tourSchema.pre('save', function (next) {
     this.slug = slugify(this.name, {
         lower: true,
     });
+    next();
+});
+
+tourSchema.pre('save', async function (next) {
+    const guidesPromise = this.guides.map(
+        async (id) => await User.findById(id)
+    );
+    this.guides = await Promise.all(guidesPromise);
     next();
 });
 
