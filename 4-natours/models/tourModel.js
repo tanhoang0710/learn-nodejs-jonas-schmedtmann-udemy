08b506e1d62cf06ea426e957e6a7d450206c4b43
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-03 08:43:08 +0700

    152 Modelling Reviews_Parent Referencing

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -117,7 +117,7 @@ const tourSchema = new mongoose.Schema(
         guides: [
             {
                 type: mongoose.Schema.ObjectId,
-                ref: 'user',
+                ref: 'User',
             },
         ],
     },
