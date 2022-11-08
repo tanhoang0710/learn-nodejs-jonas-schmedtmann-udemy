f5615c3393145d5490dba1a4c4525089717e3b2a
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-08 11:52:58 +0700

    166 Calculating Average Rating on Tours - Part 1

--- 4-natours/models/reviewModel.js
+++ 4-natours/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 // review / rating /createAt / ref to Tour / ref to user
 const reviewSchema = new mongoose.Schema(
@@ -48,6 +49,39 @@ reviewSchema.pre(/^find/, function (next) {
             select: 'name photo',
         });
     next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: {
+                tour: tourId,
+            },
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' },
+            },
+        },
+    ]);
+
+    console.log(stats);
+
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingQuantity: stats[0].nRating,
+        ratingAverage: stats[0].avgRating,
+    });
+};
+
+reviewSchema.post('save', function () {
+    // this points to current review
+
+    // vì calcAverageRatings là hàm static nên cần Model để gọi
+    // dùng this.constructor để trỏ đến Model
+    // ko để dưới hàm 87 được vì lúc đấy reviewSchema sẽ ko áp dụng hàm post
+    this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
