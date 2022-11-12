dd0b2da7d719695b3552d9ba5831a2b3b4e263e8
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-12 09:16:27 +0700

    175 First Steps With Pug

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
 
 // 3) Routes
 app.get('/', (req, res, next) => {
-    res.status(200).render('base');
+    res.status(200).render('base', { tour: 'The Forest Hiker', user: 'Jonas' });
 });
 
 app.use('/api/v1/tours', tourRouter);
