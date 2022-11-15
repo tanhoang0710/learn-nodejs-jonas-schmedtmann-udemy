09be254878c342efb121dd9143d15f915649e136
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-15 14:36:23 +0700

    184 Including a Map with Mapbox - Part1

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -25,7 +25,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
     // 3) Render template using data from 1)
     res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour',
+        title: `${tour.name} tour`,
         tour,
     });
 });
