f92552718eca4d85888e09956b5bae4da20f366a
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-16 10:20:37 +0700

    188 Logging in Users with Our API - Part 2

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -132,6 +132,38 @@ exports.protect = catchAsync(async (req, res, next) => {
     next();
 });
 
+// Only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+    if (req.cookies.jwt) {
+        // 1) Verification token
+        const decoded = await promisify(jwt.verify)(
+            req.cookies.jwt,
+            process.env.JWT_SECRET
+        );
+        // console.log(decoded);
+
+        // 2) Check if user still exists
+        const currentUser = await User.findById(decoded.id);
+        if (!currentUser) {
+            return next();
+        }
+
+        // 3) Check if user changed password after the token was issued
+        if (currentUser.changedPasswordAfter(decoded.iat)) {
+            return next();
+        }
+
+        // THERE IS A LOGGED IN USER
+        res.locals.user = currentUser;
+        console.log(
+            '🚀 ~ file: authController.js ~ line 104 ~ exports.protect=catchAsync ~ currentUser',
+            currentUser
+        );
+        return next();
+    }
+    next();
+});
+
 exports.retrictTo =
     (...roles) =>
     (req, res, next) => {
--- 4-natours/routes/viewRoutes.js
+++ 4-natours/routes/viewRoutes.js
@@ -4,9 +4,11 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.isLoggedIn);
+
 router.get('/', viewController.getOverview);
 
-router.get('/tour/:slug', authController.protect, viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 
 router.get('/login', viewController.getLoginForm);
 
