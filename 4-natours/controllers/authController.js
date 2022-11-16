f848ab76759c06b5c0c27604150efd33430b0e8f
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-16 14:14:30 +0700

    190 Logging out Users

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -77,6 +77,17 @@ exports.login = catchAsync(async (req, res, next) => {
     // });
 });
 
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        status: 'success',
+    });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
     // 1) Getting token and check if it's there
     let token;
@@ -133,36 +144,40 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 // Only for rendered pages, no errors!
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
     if (req.cookies.jwt) {
-        // 1) Verification token
-        const decoded = await promisify(jwt.verify)(
-            req.cookies.jwt,
-            process.env.JWT_SECRET
-        );
-        // console.log(decoded);
+        try {
+            // 1) Verification token
+            const decoded = await promisify(jwt.verify)(
+                req.cookies.jwt,
+                process.env.JWT_SECRET
+            );
+            // console.log(decoded);
 
-        // 2) Check if user still exists
-        const currentUser = await User.findById(decoded.id);
-        if (!currentUser) {
+            // 2) Check if user still exists
+            const currentUser = await User.findById(decoded.id);
+            if (!currentUser) {
+                return next();
+            }
+
+            // 3) Check if user changed password after the token was issued
+            if (currentUser.changedPasswordAfter(decoded.iat)) {
+                return next();
+            }
+
+            // THERE IS A LOGGED IN USER
+            res.locals.user = currentUser;
+            console.log(
+                '🚀 ~ file: authController.js ~ line 104 ~ exports.protect=catchAsync ~ currentUser',
+                currentUser
+            );
+            return next();
+        } catch (err) {
             return next();
         }
-
-        // 3) Check if user changed password after the token was issued
-        if (currentUser.changedPasswordAfter(decoded.iat)) {
-            return next();
-        }
-
-        // THERE IS A LOGGED IN USER
-        res.locals.user = currentUser;
-        console.log(
-            '🚀 ~ file: authController.js ~ line 104 ~ exports.protect=catchAsync ~ currentUser',
-            currentUser
-        );
-        return next();
     }
     next();
-});
+};
 
 exports.retrictTo =
     (...roles) =>
