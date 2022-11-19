85ad9fe768d641e051542cb83e24fab8128a9578
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-19 14:32:02 +0700

    197 Saving Image Name to Database

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
         lowercase: true, // transform, not validator
         validate: [validator.isEmail, 'Please provide a valid email'],
     },
-    photo: String,
+    photo: { type: String, default: 'default.jpg' },
     role: {
         type: String,
         enum: ['user', 'guide', 'lead-guide', 'admin'],
