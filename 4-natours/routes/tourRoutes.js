b2fd60d2fc8af135accf13503ee8ef7f1b2b3c0e
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-22 10:15:39 +0700

    200 Uploading Mutiple Images_Tours

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -59,6 +59,8 @@ router
     .patch(
         authController.protect,
         authController.retrictTo('admin', 'lead-guide'),
+        tourController.uploadTourImages,
+        tourController.resizeTourImages,
         tourController.updateTour
     )
     .delete(
