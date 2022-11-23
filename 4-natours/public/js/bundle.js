0b2d5ce894f129edd486702e9c68e23ec2cb4cd0
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-23 09:19:55 +0700

    202 Building a Complex Email Handler

--- 4-natours/public/js/bundle.js
+++ 4-natours/public/js/bundle.js
@@ -12950,7 +12950,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50282" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57046" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- 4-natours/utils/email.js
+++ 4-natours/utils/email.js
@@ -1,33 +1,56 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { convert } = require('html-to-text');
 
-const sendEmail = async (options) => {
-    // 1) Create a transporter
-    const transporter = nodemailer.createTransport({
-        // service: "Gmail",
-        // auth: {
-        //     user: process.env.EMAIL_USERNAME,
-        //     pass: process.env.EMAIL_PASSWORD
-        // }
-        // Activate in gmail "less secure app" option
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    });
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `tanhun <${process.env.EMAIL_FROM}>`;
+    }
 
-    // 2) Define the email options
-    const mailOptions = {
-        from: 'tanhun <tanhun@dev.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-        // html:
-    };
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // Sendgrid
+            return 1;
+        }
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+    }
 
-    // 3) Actually send the email
-    await transporter.sendMail(mailOptions);
+    // Send the actual email
+    async send(template, subject) {
+        // 1) Render HTML based on apug template
+        const html = pug.renderFile(
+            `${__dirname}/../views/emails/${template.pug}`,
+            {
+                firstName: this.firstName,
+                url: this.url,
+                subject,
+            }
+        );
+
+        // 2) Define the email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            text: convert(html),
+            html,
+        };
+
+        // 3) Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.send('Welcome', 'Welcome to the Natours Family');
+    }
 };
-
-module.exports = sendEmail;
