23ca6ad927d6c7471372b7afb0009d574a4e733d
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-24 10:40:19 +0700

    207 Integrating Stripe into the Back-End

--- 4-natours/public/js/bundle.js
+++ 4-natours/public/js/bundle.js
@@ -12950,7 +12950,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57046" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60822" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
