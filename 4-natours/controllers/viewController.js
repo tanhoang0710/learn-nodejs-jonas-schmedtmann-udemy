64eac643180f5f380cd7a184727ed63fa75dc5a6
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-25 10:37:43 +0700

    211 Rendering a User's Booked Tours

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
@@ -51,3 +52,17 @@ exports.getAccount = (req, res) => {
         title: 'Your account',
     });
 };
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+    // 1) Find all bookings
+    const bookings = await Booking.find({ user: req.user.id });
+
+    // 2) Find tours with the returned IDs
+    const tourIDs = bookings.map((el) => el.tour);
+    const tours = await Tour.find({ _id: { $in: tourIDs } });
+
+    res.status(200).render('overview', {
+        title: 'My Tours',
+        tours,
+    });
+});
